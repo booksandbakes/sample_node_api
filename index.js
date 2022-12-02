edb30c764ab9d66037c9cba5501b541ed04b449d
loganayaki <nayaki12112000@gmail.com>
2022-12-02 13:00:28 +0530

    first phase

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  let query = req.query;
+  console.log(query);
+  res.send(query);
+});
+
+app.post('/', function (req, res) {
+  let body_data = req.body;
+  console.log(req.body);
+  res.send(body_data);
+});
+
+app.put('/', function (req, res) {
+  let body_data = req.body;
+  console.log(req.body);
+  res.send(body_data);
+});
+
+app.patch('/', function (req, res) {
+  let body_data = req.body;
+  console.log(body_data);
+  res.send(body_data);
+});
+
+app.listen(8000, () => {
+  console.log('Starting server........');
+});
