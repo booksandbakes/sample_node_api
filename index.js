3767779cd89db163e5615e20fcd235d61910b62e
loganayaki <nayaki12112000@gmail.com>
2022-12-05 23:57:38 +0530

    authentication

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  let query = req.query;
-  if (req.headers.auth != '18pwxx') {
-    res.send({
-      auth: 'Not authenticated to use:  ' + req.headers.auth,
-    });
+app.post('/api/login', function (req, res) {
+  const user = req.body;
+  jwt.sign({ user: user }, 'secretkey', (err, token) => {
+    res.json({ token });
+  });
+});
+
+app.post('/api/post', verifytoken, function (req, res) {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ msg: 'post created', authData });
+    }
+  });
+
+  //res.send({ msg: 'post_api' });
+});
+
+function verifytoken(req, res, next) {
+  var auth = req.headers['auth'];
+  if (auth != null) {
+    req.token = auth;
+    next();
   } else {
-    //console.log(query, req.headers.auth);
-    res.send(query);
+    res.send({ msg: 'Not authorized' });
   }
-});
-
-app.post('/', function (req, res) {
-  let body_data = req.body;
-  res.send(body_data);
-});
-
-app.put('/', function (req, res) {
-  let body_data = req.body;
-  body_data.id = '18pw00';
-  res.send(body_data);
-});
-
-app.patch('/', function (req, res) {
-  let body_data = req.body;
-  body_data.id = '21xw16';
-  res.send(body_data);
-});
+}
 
 app.listen(8000, () => {
   console.log('Starting server........');
